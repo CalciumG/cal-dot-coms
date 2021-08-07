9c5ad87a424646b33ae7811d866d369c40995b7c
Callum <callumgo@outlook.com>
2021-08-08 00:40:38 +0100

    form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -31,42 +31,64 @@ class ContactForm extends React.Component {
   render() {
     const { name, email, message } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-            />
+      <form
+        netlify
+        name="contact"
+        onSubmit={this.handleSubmit}
+        className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+          Hire Me
+        </h2>
+        <p className="leading-relaxed mb-5">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+          suscipit officia aspernatur veritatis. Asperiores, aliquid?
+        </p>
+        <div className="relative mb-4">
+          <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+            Name
           </label>
-        </p>
-        <p>
-          <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            value={name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+            Email
           </label>
-        </p>
-        <p>
-          <label>
-            Message:{" "}
-            <textarea
-              name="message"
-              value={message}
-              onChange={this.handleChange}
-            />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="relative mb-4">
+          <label htmlFor="message" className="leading-7 text-sm text-gray-400">
+            Message
           </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            value={message}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-teal-500 border-0 py-2 px-6 focus:outline-none hover:bg-teal-700 rounded text-lg"
+        >
+          Submit
+        </button>
       </form>
     );
   }
