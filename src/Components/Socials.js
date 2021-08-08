eaafc8eb41415b1ac8be9f9a491424dcc9242576
Callum <callumgo@outlook.com>
2021-08-09 00:29:55 +0100

    Added pluralsight link

--- src/Components/Socials.js
+++ src/Components/Socials.js
@@ -1,27 +1,35 @@
-import React from 'react';
-import { Github, Linkedin } from "react-bootstrap-icons";
+import React from "react";
+import { Github, Linkedin, BarChartFill } from "react-bootstrap-icons";
 
 const Socials = () => {
-    return (
-        <div class="flex justify-center">
-            <a
-              class="mr-4 icon"
-              href="https://github.com/calciumg"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Github color="rgba(243,244,246)" size={40} />
-            </a>
-            <a
-              class="icon mr-4"
-              href="https://www.linkedin.com/in/callum-j-gordon/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Linkedin color="rgba(243,244,246)" size={40} />
-            </a>
-        </div>
-    );
+  return (
+    <div class="flex justify-center">
+      <a
+        class="mr-4 icon"
+        href="https://github.com/calciumg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Github color="rgba(243,244,246)" size={40} />
+      </a>
+      <a
+        class="icon mr-4"
+        href="https://www.linkedin.com/in/callum-j-gordon/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Linkedin color="rgba(243,244,246)" size={40} />
+      </a>
+      <a
+        class="icon mr-4"
+        href="https://app.pluralsight.com/profile/callum-gordon-4a"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <BarChartFill color="rgba(243,244,246)" size={40} />
+      </a>
+    </div>
+  );
 };
 
 export default Socials;
